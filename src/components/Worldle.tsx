d071a5fa3c88473882e496954ae010980a478e2c
Øyvind Solheim <oyvindbs@gmail.com>
2025-09-11 12:23:34 +0200

    valgle logo

--- src/components/Worldle.tsx
+++ src/components/Worldle.tsx
@@ -3,7 +3,12 @@ import React from "react";
 export function Worldle() {
   return (
     <span className="font-bold">
-      KOMMUND<span className="text-green-600">L</span>E
+              <span style={{color:"#EB4040"}}>V</span>
+              <span style={{color:"#E11926"}}>A</span>
+              <span style={{color:"#347103"}}>L</span>
+              <span style={{color:"#00853D"}}>G</span>
+              <span style={{color:"#87ADD7"}}>L</span>
+              <span style={{color:"#00F80"}}>E</span>
     </span>
   );
 }
